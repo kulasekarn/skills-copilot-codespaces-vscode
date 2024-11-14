07dc364e926c93b5f4ae95d0bbad015968becb37
Narayanan Kulasekar <narayanan.kulasekar@infomagnus.com>
2024-11-14 22:04:09 +0000

    Updated payments list to be generated from query table.

--- models/marts/core/int_payments__pivoted.sql
+++ models/marts/core/int_payments__pivoted.sql
@@ -3,24 +3,30 @@
         materialized='table'
     )
 }}
-{%- set payment_methods = ['bank_transfer', 'coupon', 'credit_card', 'gift_card'] -%}
+    {%- set select_query -%}
+        select distinct payment_method 
+        from {{ ref('stg_payments') }}
+        where payment_status = 'success'
+    {%- endset -%}
+
+    {%- if execute -%}
+        {%- set pymt_methods = run_query(select_query).columns[0].values() -%}   
+        {{ log('List of payment methods: ' ~ pymt_methods, info=True) }} 
+    {%- else -%}
+        {%- set pymt_methods = [] -%}   
+        {{ log('List of payment methods: ' ~ pymt_methods, info=True) }} 
+    {%- endif -%}
+
 with payments as (
     
     select * from {{ ref('stg_payments') }}
-
-),
-pymnt_methods as (
-    
-    select distinct payment_method 
-    from payments
-    where payment_status = 'success'
 
 ),
 pivoted_output as (
     
     select order_id,
            
-           {% for payment_method in payment_methods -%}           
+           {% for payment_method in pymt_methods -%}           
                 sum(case when payment_method = '{{ payment_method }}' then payment_amount else 0 end) as {{ payment_method }}_amount 
                     {%- if not loop.last -%} , {% endif %}
            {% endfor %}           
